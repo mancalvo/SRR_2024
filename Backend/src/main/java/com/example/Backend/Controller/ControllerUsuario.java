758b776418083401dfe62944b585e8105e2752b1
Dardo <dardosanchez2000@gmail.com>
2024-11-17 22:03:12 -0300

    Bedel2

--- Backend/src/main/java/com/example/Backend/Controller/ControllerUsuario.java
+++ Backend/src/main/java/com/example/Backend/Controller/ControllerUsuario.java
@@ -1,7 +1,7 @@
 package com.example.Backend.Controller;
 
+import com.example.Backend.DTO.BedelDTO;
 import com.example.Backend.DTO.UsuarioDTO;
-import com.example.Backend.Exceptions.BedelException;
 import com.example.Backend.Gestores.GestorAdministrador;
 import com.example.Backend.Gestores.GestorBedel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,30 +9,49 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios")
 public class ControllerUsuario {
 
-    private final GestorAdministrador gestorAdministrador;
-    private final GestorBedel gestorBedel;
+    @Autowired
+    private GestorAdministrador gestorAdministrador;
 
     @Autowired
-    public ControllerUsuario(GestorAdministrador gestorAdministrador, GestorBedel gestorBedel) {
-        this.gestorAdministrador = gestorAdministrador;
-        this.gestorBedel = gestorBedel;
-    }
+    private GestorBedel gestorBedel;
 
     @PostMapping("/crear")
     public ResponseEntity<String> crearUsuario(@RequestBody UsuarioDTO usuarioDTO) {
-        switch (usuarioDTO.getTipoUsuario()) {
-            case ADMINISTRADOR:
-                Integer idAdmin = gestorAdministrador.registrarNuevoAdministrador(usuarioDTO);
-                return ResponseEntity.status(HttpStatus.CREATED).body("Administrador creado correctamente con el id: " + idAdmin);
-            case BEDEL:
-                Integer idBedel = gestorBedel.registrarNuevoBedel(usuarioDTO);
-                return ResponseEntity.status(HttpStatus.CREATED).body("Bedel creado correctamente con el id: " + idBedel);
-            default:
-                throw new BedelException("Tipo de Usuario incorrecto");
-        }
+        Integer idAdmin = gestorAdministrador.registrarNuevoUsuario(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(usuarioDTO.getTipoUsuario() + " creado correctamente con el id: " + idAdmin);
+    }
+
+    @GetMapping("/bedel/{id}")
+    public ResponseEntity<BedelDTO> buscarBedel(@PathVariable Integer id) {
+        BedelDTO bedelDTO = gestorBedel.buscarBedelPorId(id);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(bedelDTO);
+    }
+
+    @GetMapping("/bedels")
+    public ResponseEntity<List<BedelDTO>> obtenerTodosLosBedels() {
+        List<BedelDTO> bedels = gestorBedel.obtenerTodosLosBedels();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(bedels);
+    }
+
+
+    @PutMapping("/bedel/{id}")
+    public ResponseEntity<String> modificarBedel(@PathVariable Integer id, @RequestBody BedelDTO bedelDTO) {
+        gestorBedel.actualizarBedel(id, bedelDTO);
+        return ResponseEntity.ok("Bedel con ID " + id + " actualizado correctamente.");
+    }
+
+
+    @DeleteMapping("/bedel/{id}")
+    public ResponseEntity<String> eliminarBedel(@PathVariable Integer id) {
+        gestorBedel.eliminarBedel(id);
+        return ResponseEntity.ok("Bedel con ID " + id + " eliminado correctamente.");
     }
 }
+
