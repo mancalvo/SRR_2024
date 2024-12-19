4d63bf040897a4e1d59036221d10d5bfd8b025a4
Dardo <dardosanchez2000@gmail.com>
2024-12-19 17:17:15 -0300

    contrasenias

--- Backend/src/main/java/com/example/Backend/Gestores/Externos/GestorContrasenia.java
+++ Backend/src/main/java/com/example/Backend/Gestores/Externos/GestorContrasenia.java
@@ -5,11 +5,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class GestorContrasenia {
 
-    public Boolean validarPoliticasContrasenia (String contrasenia) {
-        if (contrasenia.length() < 8) {
+    private static final int LONGITUD_MINIMA = 8;
+    private static final boolean REQUIERE_SIGNOS_ESPECIALES = true;
+    private static final boolean REQUIERE_MAYUSCULA = true;
+    private static final boolean REQUIERE_DIGITO = true;
+
+    public Boolean validarPoliticasContrasenia(String contrasenia) {
+
+        if (contrasenia.length() < LONGITUD_MINIMA) {
+            return false;
+        }
+
+
+        if (REQUIERE_SIGNOS_ESPECIALES && !contrasenia.matches(".*[@#$%&*].*")) {
+            return false;
+        }
+
+
+        if (REQUIERE_MAYUSCULA && !contrasenia.matches(".*[A-Z].*")) {
+            return false;
+        }
+
+
+        if (REQUIERE_DIGITO && !contrasenia.matches(".*\\d.*")) {
             return false;
         }
         return true;
     }
-
 }
